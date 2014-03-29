94dbc192254e2c4e9cb4aabb81d15d4ba7c45821
jezjestem <jjezwik@gmail.com>
2014-03-30 00:45:12 +0100

    added few options like init|update|sync

--- update.sh
+++ update.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 
-for d in $(find . -type d -maxdepth 1 -not -name ".*")
-do
-	cd $d
-	git pull origin
-	cd ..
-done
+
+case $1 in
+	update)
+		for d in $(find . -type d -maxdepth 1 -not -name ".*")
+		do
+			cd $d
+			git pull origin
+			cd ..
+		done
+	;;
+	
+	sync)
+		for d in $(find . -type d -maxdepth 1 -not -name ".*")
+		do
+			cd $d
+			git push origin master
+			cd ..
+		done
+	;;
+	init)
+		mkdir -m 700 -p ~/code ; cd ~/code
+
+		git clone https://github.com/jezjestem/sysadmin.git
+		git clone https://github.com/jezjestem/digitalhoryzont.git
+	;;
+	*)
+		echo "usage: $(basename $0) [update|sync|init]"
+		exit 64;
+		# -.
+	;;
+esac
+
